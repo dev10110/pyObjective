442c43f158eafa97c92669ffe3d8bf2945866052
Dev <dra16@ic.ac.uk>
2019-08-05 19:36:47 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('-e.'))
 sys.path.insert(0, os.path.abspath('-e../'))
 master_doc = 'index'
-
+autodoc-mock-imports = ['scipy, prettytable']
 # -- Project information -----------------------------------------------------
 
 project = 'pyObjective'
